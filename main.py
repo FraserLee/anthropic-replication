5aaadfab67e136b31d82d0b536ffab47a94502f3
Thomas Lemoine <lemoine123thomas@gmail.com>
2024-01-23 21:09:25 -0500

    predict stuff with gpt2 in main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import torch
+from transformers import GPT2Tokenizer, GPT2LMHeadModel
 
 if torch.cuda.is_available():
     print("device count:", torch.cuda.device_count())
@@ -7,3 +8,35 @@ if torch.cuda.is_available():
 else:
     print("torch run on cpu")
 
+# Load pre-trained model tokenizer (vocabulary)
+tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+
+# Encode a text inputs
+text = "What is the fastest car in the country of "
+indexed_tokens = tokenizer.encode(text)
+
+# Convert indexed tokens in a PyTorch tensor
+tokens_tensor = torch.tensor([indexed_tokens])
+
+# Load pre-trained model (weights)
+model = GPT2LMHeadModel.from_pretrained('gpt2')
+
+# Set the model in evaluation mode to deactivate the DropOut modules
+model.eval()
+
+# # If you have a GPU, put everything on cuda
+if torch.cuda.is_available():
+    tokens_tensor = tokens_tensor.to('cuda')
+    model.to('cuda')
+
+# Predict all tokens
+with torch.no_grad():
+    outputs = model(tokens_tensor)
+    predictions = outputs[0]
+
+# Get the predicted next sub-word
+predicted_index = torch.argmax(predictions[0, -1, :]).item()
+predicted_text = tokenizer.decode(indexed_tokens + [predicted_index])
+
+# Print the predicted word
+print(predicted_text)
