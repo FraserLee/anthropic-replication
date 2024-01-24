b96125b646f451e85621522d98ad0ae12d384d60
Fraser <tomatoesneverlie@gmail.com>
2024-01-23 19:32:31 -0500

    more info in main

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import torch
 
 if torch.cuda.is_available():
-    print(torch.cuda.current_device())
+    print("device count:", torch.cuda.device_count())
+    print("current device:", torch.cuda.current_device())
+    print("device name:", torch.cuda.get_device_name(torch.cuda.current_device()))
 else:
     print("torch run on cpu")
 
