0d415171e7167e9e6cb21eef09185a2959700b2c
Fraser <tomatoesneverlie@gmail.com>
2024-02-18 16:44:54 -0500

    tokenize dataset

--- src/dataset.py
+++ src/dataset.py
@@ -1,24 +1,32 @@
 import datasets
+import tiktoken
 import os
+
+enc = tiktoken.get_encoding("cl100k_base")
 
 def get_dataset():
 
     dataset_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../cache/dataset_cache'))
-    print(f"dataset_path: {dataset_path}")
     if not os.path.exists(dataset_path):
         os.makedirs(dataset_path)
 
-    # datasets.logging.set_verbosity(datasets.logging.ERROR)
-    datasets.logging.set_verbosity(datasets.logging.INFO)
+    datasets.logging.set_verbosity(datasets.logging.ERROR)
+    # datasets.logging.set_verbosity(datasets.logging.INFO)
 
-    train = datasets.load_dataset("c4", "en", streaming = True, split = "train")
-    val   = datasets.load_dataset("c4", "en", streaming = True, split = "validation")
+    train = datasets.load_dataset("c4", "en", streaming = True, trust_remote_code = True, split = "train")
+    val   = datasets.load_dataset("c4", "en", streaming = True, trust_remote_code = True, split = "validation")
 
-    # get just the text part of the dataset
+    # filter to only text column
     train = map(lambda x: x['text'], train)
     val   = map(lambda x: x['text'], val)
 
-    print(next(iter(train)))
+    # tokenize
+    train = map(lambda x: enc.encode(x), train)
+    val   = map(lambda x: enc.encode(x), val)
+
+    x = next(iter(train))
+    print(x)
+    print(enc.decode(x))
 
     return train, val
 
